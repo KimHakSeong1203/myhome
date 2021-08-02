f31d8597dba1e9aa0fbfad7cd2bf30567c2b579e
KimHakSeong1203 <rmsp0211@gmail.com>
2021-08-02 17:55:48 +0900

    layout 구성

---
+++ src/main/java/com/palap/myhome/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.palap.myhome.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/index.html")
+    public String index() {
+        return "index";
+    }
+}
