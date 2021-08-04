351ebd2c7174190ebdc9d9ce99a358184a9c2d4f
KimHakSeong1203 <rmsp0211@gmail.com>
2021-08-04 20:03:05 +0900

    08.04

---
+++ src/main/java/com/palap/myhome/controller/EmployeeRepository.java
@@ -0,0 +1,2 @@
+package com.palap.myhome.controller;public class EmployeeRepository {
+}
--- src/main/java/com/palap/myhome/repository/BoardRepository.java
+++ src/main/java/com/palap/myhome/repository/BoardRepository.java
@@ -3,6 +3,9 @@ package com.palap.myhome.repository;
 import com.palap.myhome.model.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BoardRepository extends JpaRepository <Board, Long> {
+import java.util.List;
 
+public interface BoardRepository extends JpaRepository <Board, Long> {
+    List<Board> findByTitle(String title);
+    List<Board> findByTitleOrContent(String title, String content);
 }
